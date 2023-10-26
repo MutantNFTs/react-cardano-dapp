7a6c4b4a4d76793f47b0e82648454c5a3a956529
Geraldo Neto <geraldo.lopes@outlook.com>
2023-10-26 20:07:24 +0100

    fix: simplify useRequestUTxOs

--- src/hooks/useRequestUTxOsByValue.ts
+++ src/hooks/useRequestUTxOsByValue.ts
@@ -2,17 +2,26 @@ import { selectUtxosByValue, Value } from "@mutants/cardano-tx-builder";
 
 import { useUTxOs } from "./useUTxOs";
 
+import { CardanoWallet } from "../CardanoWallet";
+
 export const useRequestUTxOsByValue = () => {
   const { utxos, refresh } = useUTxOs();
 
   return async (requestedValue: Value) => {
-    let result = selectUtxosByValue(utxos, requestedValue);
+    const availableUtxos = utxos.length
+      ? utxos
+      : await CardanoWallet.getUTxOs();
+
+    let result = selectUtxosByValue(availableUtxos, requestedValue);
 
     if (result.missing) {
       // take another shot since decoded UTxOs are cached.
       await refresh();
 
-      result = selectUtxosByValue(utxos, requestedValue);
+      result = selectUtxosByValue(
+        await CardanoWallet.getUTxOs(),
+        requestedValue
+      );
     }
 
     return result;
